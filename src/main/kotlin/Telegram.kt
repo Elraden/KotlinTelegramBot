a09df47e5ca57790cb1e38ff7011c204be2bb7a3
Elraden <morozovsa94@yandex.ru>
2025-02-11 16:56:27 +0300

    KTB-16 created buttons and send message to user

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -1,14 +1,20 @@
 package org.example
 
+const val TG_URL = "https://api.telegram.org/bot"
 fun main(args: Array<String>) {
     val botToken = args[0]
     val botService = TelegramBotService(botToken)
 
     var updateId = 0
     val defaultMessage = "no messages"
+    val defaultData = "no data"
+
     val regexUpdateId = Regex("\"update_id\":(\\d+)")
     val regexMessageText = Regex("\"text\":\"(.+?)\"")
     val regexChatId = Regex("\"chat\":\\{\"id\":(\\d+)")
+    val regexData = Regex("\"data\":\"(.+?)\"")
+
+    val trainer = LearnWordsTrainer("words.txt")
 
     while (true) {
         Thread.sleep(2000)
@@ -22,10 +28,7 @@ fun main(args: Array<String>) {
             ?.toIntOrNull()
             ?.plus(1) ?: continue
 
-        val messageText = regexMessageText
-            .find(updates)
-            ?.groups
-            ?.get(1)?.value ?: defaultMessage
+        val messageText = regexMessageText.find(updates)?.groups?.get(1)?.value ?: defaultMessage
 
         println(messageText)
 
@@ -37,8 +40,16 @@ fun main(args: Array<String>) {
 
         println(chatId)
 
-        if (messageText == "Hello") {
+        val data = regexData.find(updates)?.groups?.get(1)?.value ?: defaultData
+
+        println(data)
+
+        if (messageText.lowercase() == "hello") {
             botService.sendMessage(chatId, "Hello")
+        }
+
+        if (messageText.lowercase() == "/start") {
+            botService.sendMenu(chatId)
         }
     }
 
